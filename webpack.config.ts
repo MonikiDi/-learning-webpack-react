805f81da27e4182b3f6445f81227dc2a7dd20e87
Denis Grigorev <d1e9n9i2s@gmail.com>
2023-10-14 22:23:27 +0300

    webpack build template

---
+++ webpack.config.ts
@@ -0,0 +1,32 @@
+import path from "path";
+import webpack from "webpack";
+import HTMLWebpackPlugin from "html-webpack-plugin";
+
+const config: webpack.Configuration = {
+  entry: path.resolve(__dirname, "src", "index.ts"),
+  output: {
+    filename: "[name].[contenthash].js",
+    path: path.resolve(__dirname, "dist"),
+    clean: true,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: "ts-loader",
+        exclude: /node_modules/,
+      },
+    ],
+  },
+  resolve: {
+    extensions: [".tsx", ".ts", ".js"],
+  },
+  plugins: [
+    new webpack.ProgressPlugin(),
+    new HTMLWebpackPlugin({
+      template: path.resolve(__dirname, "public", "index.html"),
+    }),
+  ],
+};
+
+export default config;
