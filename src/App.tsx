a598c23e8377acbabdaef85199a78b7b5a31ebb3
Denis Grigorev <d1e9n9i2s@gmail.com>
2023-10-15 20:02:47 +0300

    folder structure for styles

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,11 @@ import { Route, Routes, Link } from "react-router-dom";
 import { AboutPageAsync } from "./pages/AboutPage/AboutPage.async";
 import { MainPageAsync } from "./pages/MainPage/MainPage.async";
 import { Suspense } from "react";
-import "./index.scss";
+import "./styles/index.scss";
 
 const App = () => {
   return (
-    <div className="app">
+    <div className="app dark">
       <Link to={"/"}>Главная</Link>
       <Link to={"/about"}>О сайте</Link>
       <Suspense fallback={<div>Loading...</div>}>
