60645bd15ddbc8620b49aa99aa3f7356d7009522
Denis Grigorev <d1e9n9i2s@gmail.com>
2023-10-15 21:03:46 +0300

    implemented a theme switching function

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,16 @@
+import { Suspense} from "react";
 import { Route, Routes, Link } from "react-router-dom";
+import "./styles/index.scss";
 import { AboutPageAsync } from "./pages/AboutPage/AboutPage.async";
 import { MainPageAsync } from "./pages/MainPage/MainPage.async";
-import { Suspense } from "react";
-import "./styles/index.scss";
+import { useTheme } from './theme/useTheme';
 
 const App = () => {
+  const { theme, toggleTheme } = useTheme();
+
   return (
-    <div className="app dark">
+    <div className={`app ${theme}`}>
+      <button onClick={toggleTheme}>TOGGLE</button>
       <Link to={"/"}>Главная</Link>
       <Link to={"/about"}>О сайте</Link>
       <Suspense fallback={<div>Loading...</div>}>
