167b731ed699a66e53b9554ba1d35a72b3edd78b
Denis Grigorev <d1e9n9i2s@gmail.com>
2023-10-15 16:19:39 +0300

    implemented css modules

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import { Counter } from "./components/Counter";
+import './index.scss';
+
+const App = () => {
+  return (
+    <div className="app">
+      Lorem ipsum dolor, sit amet consectetur adipisicing elit. Similique, nulla commodi. Assumenda quaerat ratione excepturi, ab molestias cum quo maxime laboriosam rem! Quo repellendus nesciunt officia facere assumenda odit aspernatur.
+      <Counter />
+    </div>
+  );
+};
+export default App;
